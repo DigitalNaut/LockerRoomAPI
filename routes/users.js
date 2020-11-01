5134e525c83e4bacb8c65f16f550b997f39e0329
Cybernaut <77c828fa@opayq.com>
2020-11-01 05:02:55 -0600

    Fix: Uncommented code used for debugging

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ const user = require('../controllers/users');
 router.get('/', user.show_users);
 
 /* API */
-// router.get('/:id', user.show_user);
+router.get('/:id', user.show_user);
 router.post('/new', user.new_user);
-// router.put('/:id/edit', user.edit_user);
-// router.delete('/:id/remove', user.remove_user);
+router.put('/:id/edit', user.edit_user);
+router.delete('/:id/remove', user.remove_user);
 
 module.exports = router;
